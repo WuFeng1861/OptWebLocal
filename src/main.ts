2a964bb06df014ff5820f80b5d02572efb85c75d
WuFeng1861 <71131991+WuFeng1861@users.noreply.github.com>
2025-06-10 01:40:22 +0800

    Updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,4 @@ createApp(App)
   .use(ElementPlus)
   .mount('#app')
   .$nextTick(() => {
-  // Use contextBridge
-  window.ipcRenderer.on('main-process-message', (_event, message) => {
-    console.log(message)
-  })
 })
